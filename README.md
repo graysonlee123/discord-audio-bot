c66eef7aaf341db6086065eec3af9b628d13db38
graysonlee123 <graysonleegantek@gmail.com>
2020-04-14 19:54:19 -0400

    adds axios; makes request to youtube api; searches for most relevant search term and plays the top result

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-**Requirements** 
+**Requirements**
 
-See the file `.example.config.json` (to hide your login for the bot). You __must create this file__ to run the bot. If you are unfamiliar with creating discord bots: https://lmgtfy.com/?q=Discord+Bots
+See the file `.example.config.json` (to hide your login for the bot). You **must create this file** to run the bot. If you are unfamiliar with creating discord bots: https://lmgtfy.com/?q=Discord+Bots
 
 **Starting**
 
-Simply run `node .` to start the bot, after installing packages and setting up a auth.json file.
+Simply run `node index.js` to start the bot, after installing packages `npm install` and setting up the `config.json` file. You will need your Discord bot's token, and a YouTube API key.
 
 **auth.json Example**
 
@@ -12,5 +12,6 @@ See the file `.example.config.json`.
 
 **Documentation**
 
-* All GUILD methods and properties: [https://discord.js.org/#/docs/main/stable/class/Guild]
-* All USER methods and properties: [https://discord.js.org/#/docs/main/stable/class/User]
+- All GUILD methods and properties: [https://discord.js.org/#/docs/main/stable/class/Guild]
+- All USER methods and properties: [https://discord.js.org/#/docs/main/stable/class/User]
+- Google API: [https://developers.google.com/youtube/v3/docs/search/list]
--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,6 @@
 const ytdl = require('ytdl-core');
+const axios = require('axios');
+const { youtubeApiKey } = require('../config.json');
 
 module.exports = {
   name: 'play',
@@ -8,17 +10,18 @@ module.exports = {
   voiceConnected: true,
   async execute(message, args) {
     const connection = await message.member.voice.channel.join();
-    const search = args[0];
+    const search = args.join(' ');
     const regex = RegExp('^(http||https)://.+');
+    const options = {
+      quality: 'highestaudio',
+    };
 
     if (regex.test(search)) {
       // Is a url
       try {
-        const verifyVideo = await ytdl.getBasicInfo(search);
+        await ytdl.getBasicInfo(search);
 
-        connection.play(ytdl(search), {
-          quality: 'highestaudio',
-        });
+        connection.play(ytdl(search), options);
       } catch (err) {
         return message.channel.send(
           `That video URL was not valid, ${message.author}! \nError: ${err.message}`
@@ -26,7 +29,28 @@ module.exports = {
       }
     } else {
       // Search youtube
-      message.channel.send(`Searching youtube... (need to make)`);
+      const baseurl = `https://www.googleapis.com/youtube/v3/search`;
+      const query = `?part=snippet&q=${search}&key=${youtubeApiKey}&maxResults=1`;
+
+      console.log(baseurl, query);
+
+      message.channel.send(`Searching youtube for "${search}"`);
+      try {
+        const res = await axios.get(`${baseurl}${query}`);
+        const videoId = res.data.items[0].id.videoId;
+
+        await ytdl.validateID(videoId);
+
+        const youtubeLink = `https://www.youtube.com/watch?v=${videoId}`;
+
+        message.channel.send(`This is what I found: ${youtubeLink}`);
+
+        connection.play(ytdl(youtubeLink), options);
+      } catch (err) {
+        return message.channel.send(
+          `There was an error with that YouTube search, ${message.author}. \nError: ${err.message}`
+        );
+      }
     }
   },
 };
