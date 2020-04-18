2aae024c393203b9e489a5caca87289ff2827c98
graysonlee123 <graysonleegantek@gmail.com>
2020-04-18 19:33:51 -0400

    updates stop command to play nicer with the console

--- commands/stop.js
+++ commands/stop.js
@@ -1,5 +1,3 @@
-const { queue } = require('../index');
-
 module.exports = {
   name: 'stop',
   aliases: ['end', 'leave', 'disconnect'],
@@ -7,7 +5,12 @@ module.exports = {
   voiceConnected: true,
   description: 'Remove the queue and disconnect me from the voice chat.',
   async execute(message, args, serverQueue) {
+    if (!serverQueue) return message.channel.send(`I'm not playing anything!`);
+
     serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
+
+    if (serverQueue.connection.dispatcher) {
+      serverQueue.connection.dispatcher.end();
+    }
   },
 };
