45c80cf5632344dcf126e28956afafd4f7737f39
Grayson Gantek <graysonleegantek@gmail.com>
2020-04-24 17:47:10 -0400

    adds category option to joke command

--- commands/joke.js
+++ commands/joke.js
@@ -2,13 +2,41 @@ const axios = require("axios");
 
 module.exports = {
   name: "joke",
-  description: "Get a random joke from Joke API by Sven Fehler.",
-  aliases: ["funny", "laugh"],
+  description:
+    'Get a random joke from Joke API by Sven Fehler. Joke categories include "programming", "misc", and "dark"',
+  usage: "[category]",
   async execute(message, args) {
+    let category;
+
+    switch (args[0]) {
+      case "misc":
+      case "miscellaneous":
+      case "Misc":
+      case "Miscellaneous":
+        category = "miscellaneous";
+        break;
+      case "programming":
+      case "Programming":
+        category = "programming";
+        break;
+      case "dark":
+      case "Dark":
+        category = "dark";
+        break;
+      default:
+        category = "any";
+        break;
+    }
+
+    let url = `https://sv443.net/jokeapi/v2/joke/${category}`;
+
     try {
       const res = await axios({
-        url: "https://sv443.net/jokeapi/v2/joke/any?type=single",
+        url: url,
       });
+
+      if (res.data.error)
+        return message.reply(`there was an error with that request!`);
 
       const { category, type, flags, id, error } = res.data;
 
@@ -23,10 +51,8 @@ module.exports = {
         const { joke } = res.data;
         message.channel.send(joke);
       }
-
-      console.log(res.data);
     } catch (err) {
-      return;
+      return message.reply(`there was an error running that command!`)
     }
   },
 };
